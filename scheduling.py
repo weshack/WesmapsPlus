220c7feda2ae0d717104c6aaab841587cb898a4e
Max Dietz <maxwell.r.dietz@gmail.com>
2013-05-18 17:56:11 -0400

    added multiple meetings support
    Scheduling now supports multiple class meetings in the same day

--- scheduling.py
+++ scheduling.py
@@ -1,21 +1,28 @@
+import datetime
+
 def convertTimeStringToDictionary(timeString):
 	times = timeString.split(";")[:-1]
 	schedule = {}
 	for date in times:
 		for i in range(0,7):
 			if date.strip()[i] != '.':
-				schedule[datetime.datetime(1,1,i+7).strftime('%A')] = timeRangeToMilitary(date.strip()[8:])
+				if datetime.datetime(1,1,i+7).strftime('%A') in schedule:
+					schedule[datetime.datetime(1,1,i+7).strftime('%A')] += [timeRangeToMilitary(date.strip()[8:])]
+				else:
+					schedule[datetime.datetime(1,1,i+7).strftime('%A')] = [timeRangeToMilitary(date.strip()[8:])]
 	return schedule
 	
 
 def noConflict(currSchedule, newCourse):
 	''' Tests two times, and returns True if they do not conflict '''
 	for newTimeK in newCourse:
-		newTime = newCourse[newTimeK]
-		for timeK in currSchedule:
-			time = currSchedule[TimeK]
-			if (time[0] >= newTime[0] and time[0] <= newTime[1]) or (newTime[0] >= time[0] and newTime[0] <= time[1]):
-				return False
+		newTimes = newCourse[newTimeK]
+		for newTime in newTimes:
+			for timeK in currSchedule:
+				times = currSchedule[TimeK]
+				for time in times:
+					if (time[0] >= newTime[0] and time[0] <= newTime[1]) or (newTime[0] >= time[0] and newTime[0] <= time[1]):
+						return False
 	return True
 
 def timeRangeToMilitary(timeString):
