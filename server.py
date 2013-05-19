0dcc45689c979bdfb8c546f38e3884d3034dea75
Anastasis Germanidis <agermanidis@gmail.com>
2013-05-18 23:17:31 -0400

    Starring and scheduling work perfectly

--- server.py
+++ server.py
@@ -2,10 +2,10 @@ import simplejson
 import scheduling
 
 from flask import Flask, render_template, request, jsonify, g, session
-from db import search_for_course_by_title, connect_db, get_courses_from_cursor, get_all_information
+from db import search_for_course_by_title, connect_db, get_courses_from_cursor, get_all_information, get_times_for_section
 from pymongo import MongoClient
 from user import create_user, get_user_info, update_user_schedule, update_user_starred
-from scheduling import noConflict
+from scheduling import noConflict, convertTimeStringToDictionary
 
 app = Flask(__name__)
 
@@ -33,55 +33,100 @@ def search():
 def course_info(courseid):
     return simplejson.dumps(get_all_information(g.db, courseid))
     
-@app.route('/star', methods = ['GET', 'POST', 'DELETE'])
-def update_starred():
+@app.route('/starred')
+def get_starred():
+    starred = get_user_info(session)['starred']
+    return simplejson.dumps(starred)
+
+@app.route('/star/<courseid>', methods = ['POST', 'DELETE'])
+def update_starred(courseid = ''):
     starred = set( get_user_info(session)['starred'] )
     
-    if request.method == 'GET':
-        return simplejson.dumps(list(starred))
-
-    elif request.method == 'DELETE':
-        courseid = request.form['courseid']
-        starred.add(courseid)
-        update_user_starred(starred)
-        return simplejson.dumps(list(starred))
+    if request.method == 'DELETE':
+        try:
+            starred.remove(courseid)
+        except:
+            pass
+        starred = list(starred)
+        update_user_starred(session['userid'], starred)
+        return simplejson.dumps(starred)
 
     else:
-        courseid = request.form['courseid']
         starred.add(courseid)
-        update_user_starred(starred)
-        return simplejson.dumps(list(starred))
+        starred = list(starred)
+        update_user_starred(session['userid'], starred)
+        return simplejson.dumps(starred)
 
-@app.route("/schedule", methods = ['GET', 'POST, DELETE'])
-def update_schedule():
+def merge_dictionaries(dicts):
+    ret = {}
+    for d in dicts:
+        for k, v in d.items():
+            for r in v:
+                if k in ret:
+                    ret[k].append(r)
+                else:
+                    ret[k] = [r]
+    return ret
+
+def get_sections_for_user(session):
+    userinfo = get_user_info(session)
+    sections = set(userinfo['sections'])
+    ret = {}
+
+    for section in sections:
+        ret[section] = convertTimeStringToDictionary(get_times_for_section(g.db, section))
+ 
+    return ret
+
+@app.route('/debug/sections')
+def get_sections():
     userinfo = get_user_info(session)
     sections = userinfo['sections']
+    return simplejson.dumps(sections)
 
-    if request.method == 'GET':
-        times = {}
-        for section in sections:
-            times['section'] = convertTimeStringToDictionary(get_times_for_section(g.db, section))
-        return simplejson.dumps(times)
+@app.route('/schedule/<section>', methods = ['POST', 'DELETE'])
+def handle_section(section):
+    userinfo = get_user_info(session)
+    sections = set(userinfo['sections'])
 
-    elif request.method == 'DELETE' and section in sections: 
+    if request.method == 'DELETE': 
         # Removing a course
-        section = request.form['section']
-        sections = sections[:sections.index(section)] + sections[sections.index(section)+1:]
+        print 'rargs', request.args.get('section')
+        try:
+            sections.remove(section)
+        except:
+            pass
+        sections = list(sections)
         update_user_schedule(session['userid'], sections)
-        return 200
+        return simplejson.dumps(sections)
 
     else: 
         # Adding a course
-        section = request.form['section']
         allTimes = {}
+        conflictDetected = False
+
+        if section in sections:
+            return simplejson.dumps( get_sections_for_user(session) )
+
         for s in sections:
             allTimes[s] = convertTimeStringToDictionary(get_times_for_section(g.db, s))
-            if noConflict(allTimes, convertTimeStringToDictionary(get_times_for_section(g.db, section))):
-                sections.append(section)
-            else:
-                pass
+            if not (noConflict(allTimes, convertTimeStringToDictionary(get_times_for_section(g.db, section)))):
+                conflictDetected = True
+                break
 
-    update_user_schedule(session['userid'], sections)
+        if conflictDetected:
+            return "Conflict detected", 400
+
+        else:
+            sections.add(section)
+            update_user_schedule(session['userid'], list(sections))
+            return simplejson.dumps( get_sections_for_user(session) )
+
+@app.route("/schedule")
+def get_schedule():
+    return simplejson.dumps( get_sections_for_user(session) )
+
+    
 	
 if __name__ == "__main__":
     app.secret_key = 'b6a7ab74af724b1e948b42a30c959cb8'
